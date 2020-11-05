3152d82f2c407d1cb87c7627336ee14f0826fd56
Viraj Samson <virajsamson098@gmail.com>
2020-11-04 22:25:25 -0600

    added Event business layer

---
+++ EventBusinessLayer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBusinessLayer
+{
+    public class Class1
+    {
+    }
+}
