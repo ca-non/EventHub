4edd5459d48ec8b032c11e860f6b1d29c94768d2
Viraj Samson <virajsamson098@gmail.com>
2020-11-20 22:56:41 -0600

    reset link added

--- UserBusinessLayer/UserBusinessLogic.cs
+++ UserBusinessLayer/UserBusinessLogic.cs
@@ -418,6 +418,27 @@ namespace UserBusinessLayer
                 }
             }
 
+            using
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spIsPasswordResetLinkValid", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@GUID", uid);
+
+                con.Open();
+
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        if (Convert.ToBoolean(rdr["IsValidPasswordLink"]))
+                        {
+                            return flag;
+                        }
+                    }
+                }
+            }
+
             return flag;
         }
 
